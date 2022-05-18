fb5a91da718df82ed2f42d4687c15af762011585
João David Nóbrega <jnobrega@neoception.com>
2022-05-19 00:02:41 +0100

    Done

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
+
   return response.json(repositories);
 });
 
@@ -23,32 +24,37 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  if (updatedRepository.likes > 0) {
+    updatedRepository.likes = 0;
+  }
 
-  if (repositoryIndex < 0) {
+  const repositoryExists = repositories.find(repository => repository.id === id);
+
+  if (!repositoryExists) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
-  repositories[repositoryIndex] = repository;
+  const repository = { ...repositoryExists, ...updatedRepository };
 
   return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
+
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -58,17 +64,19 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
+
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryExists = repositories.find(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
+  if (!repositoryExists) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  const likes = ++repositoryExists.likes;
 
-  return response.json('likes');
+  return response.json({ likes: likes});
+
 });
 
 module.exports = app;
